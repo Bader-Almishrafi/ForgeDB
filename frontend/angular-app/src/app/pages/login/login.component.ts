8720772e9f793d0a88b704a91bfe1b0ad2907c53
i1SaSa <s9ny398335@gmail.com>
2026-06-26 19:22:33 +0300

    feat: enhance login and signup components with password reset functionality and routing updates

--- frontend/angular-app/src/app/pages/login/login.component.ts
+++ frontend/angular-app/src/app/pages/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
 	selector: 'app-login',
-	imports: [],
+	imports: [FormsModule, RouterLink],
 	templateUrl: './login.component.html',
 	styleUrl: './login.component.css',
 })
 export class LoginComponent {
+	email: string = '';
+	password: string = '';
+	forgotEmail: string = '';
 
+	showForgotPasswordState: boolean = false;
+
+
+	onLogin() {
+		// Handle login logic here
+		console.log('Email:', this.email);
+		console.log('Password:', this.password);
+
+	}
+	onResetPassword() {
+		console.log('Reset Link Sent to:', this.forgotEmail);
+		alert('Reset link sent to your email!');
+	}
 }
