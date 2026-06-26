6f82e29c51c57907cde049669cd2d1b842d67e07
i1SaSa <s9ny398335@gmail.com>
2026-06-26 20:56:35 +0300

    feat: implement reactive form for signup with validation and data binding

--- frontend/angular-app/src/app/pages/signup/signup.component.ts
+++ frontend/angular-app/src/app/pages/signup/signup.component.ts
@@ -1,13 +1,33 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-signup',
-  imports: [RouterLink],
-  templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css',
+	selector: 'app-signup',
+	imports: [RouterLink, FormsModule],
+	templateUrl: './signup.component.html',
+	styleUrl: './signup.component.css',
 })
 export class SignupComponent {
+	fullName: string = '';
+	email: string = '';
+	password: string = '';
+	confirmPassword: string = '';
+
+
+	onRegister() {
+		if (this.password !== this.confirmPassword) {
+			alert('Error: Passwords do not match!');
+			return;
+		}
+
+		console.log('Registration Data:', {
+			name: this.fullName,
+			email: this.email,
+			password: this.password
+		});
+
+		alert('Registration successful! (Check Console)');
+	}
 
 }
